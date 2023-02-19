fee2bc50fcb3f66f6492e055349bb889490328c0
shuja <shuja.gmail.com>
2023-02-19 11:09:41 +0530

    mongo catalogue

---
+++ catalogue.sh
@@ -0,0 +1,15 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+npm install
+cp configs/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp configs/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb.shujadevops.online </app/schema/catalogue.js
