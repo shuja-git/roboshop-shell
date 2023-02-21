d95ffc9db9ba974c694989b0f8b865cb65abbd73
shuja <shuja.gmail.com>
2023-02-21 12:41:53 +0530

    Given space after m

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,17 @@
-echo "\e[35mInstalling Nginx\e[0m"
+echo "\e[35m Installing Nginx\e[0m"
 yum install nginx -y
-echo "\e[35;1mRemoving Old Contents\e[0m"
+echo "\e[35m Removing Old Contents\e[0m"
 rm -rf /usr/share/nginx/html/*
-echo "\e[35mDownloading frontend\e[0m"
+echo "\e[35m Downloading frontend\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo "\e[35mExtracting frontend\e[0m"
+echo "\e[35m Extracting frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-echo "\e[35mCopying into roboshop conf\e[0m"
+echo "\e[35m Copying into roboshop conf\e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
-echo "\e[35mEnabling Nginx\e[0m"
+echo "\e[35m Enabling Nginx\e[0m"
 systemctl enable nginx
-echo "\e[35mStarting Nginx\e[0m"
+echo "\e[35m Starting Nginx\e[0m"
 systemctl restart nginx
 
 
