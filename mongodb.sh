2f88611538660647e847752ce71f04f63bf9ee96
shuja <shuja.gmail.com>
2023-02-22 15:26:42 +0530

    print function

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,20 @@
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+print_head(){
+echo -e "\e[1;36m$1 \e[0m"
+}
+
+print_head "Setup mongodb repos"
 cp configs/mongo.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install Mongodb"
 yum install mongodb-org -y
+
+print_head "Enable mongodb"
 systemctl enable mongod
+
+print_head "Start mongodb"
 systemctl start mongod
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
