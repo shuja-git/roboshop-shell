7099bf5c9983602b0d8a8e17dc9873206f1a2e44
shuja <shuja.gmail.com>
2023-02-22 12:01:06 +0530

    space

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ echo -e "\e[1;35m Removing Old Contents\e[0m"
 rm -rf /usr/share/nginx/html/*
 echo -e "\e[1;35m Downloading frontend\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo -e"\e[1;35m Extracting frontend\e[0m"
+echo -e "\e[1;35m Extracting frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 echo -e "\e[1;35m Copying into roboshop conf\e[0m"
