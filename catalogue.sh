49a2208d4b638c29592a743f2398e0b036b502ed
shuja <shuja.gmail.com>
2023-02-22 15:46:20 +0530

    print function added

--- catalogue.sh
+++ catalogue.sh
@@ -1,17 +1,47 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+print_head "Configure Nodejs Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Install Nodejs"
+yum install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop user"
 useradd roboshop
+
+print_head "Create application directory"
 mkdir /app
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+print_head "Delete Old contents"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading app contents"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
-unzip /tmp/catalogue.zip
-npm install
+
+print_head "Extracting app contents"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+print_head "Installing Nodejs dependencies"
+npm install &>>${log_file}
+
+print_head "Copy systemd Service file"
 cp configs/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable cataloge service "
+systemctl enable catalogue &>>${log_file}
+
+print_head "Restart Catalogue service"
+systemctl restart catalogue &>>${log_file}
+
+print_head "Copy mongodb repo file"
 cp configs/mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.shujadevops.online </app/schema/catalogue.js
+
+print_head "Installing mongo client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host mongodb.shujadevops.online </app/schema/catalogue.js &>>${log_file}
