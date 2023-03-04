0f53a89898e4d97ec837c2b51178978c40b08048
shuja <shuja.gmail.com>
2023-03-04 23:17:23 +0530

    exit 1

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,7 @@ source common.sh
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
     echo -e "\e[31mMissing mysql password argument\e[0m"
+    exit 1
 fi
 
 print_head "Disable mysql 8"
