32f341021bd653abc9cfcf722d6ddf934319a941
shuja <shuja.gmail.com>
2023-03-04 15:28:14 +0530

    testing exit status

--- mongodb.sh
+++ mongodb.sh
@@ -2,25 +2,14 @@ source common.sh
 
 print_head "Setup mongodb repos" &>>${log_file}
 cp configs/mongo.repo /etc/yum.repos.d/mongo.repo
-if [ "$?" -q 0 ]; then
-    echo -e "\e[33m Success\e[0m"
-fi
+echo $?
 print_head "Install Mongodb" &>>${log_file}
 yum install mongodb-org -y
-if [ "$?" -q 0 ]; then
-    echo -e "\e[33m Success\e[0m"
-fi
-
+echo $?
 print_head "Enable mongodb" &>>${log_file}
 systemctl enable mongod
-if [ "$?" -q 0 ]; then
-    echo -e "\e[33m Success\e[0m"
-fi
-
+echo $?
 print_head "Start mongodb" &>>${log_file}
 systemctl start mongod
-if [ "$?" -q 0 ]; then
-    echo -e "\e[33m Success\e[0m"
-fi
-
+echo $?
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
