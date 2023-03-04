713b63cfe7a2d872d6568ea9e9e1aa7c7bd32271
shuja <shuja.gmail.com>
2023-03-04 15:36:38 +0530

    sed

--- mongodb.sh
+++ mongodb.sh
@@ -5,9 +5,10 @@ cp configs/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 if [ "$?" -eq 0 ]; then
     echo success
 fi
-print_head "Install Mongodb" &>>${log_file}
+print_head "Install Mongodb"
 yum install mongodb-org -y &>>${log_file}
 echo $?
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 print_head "Enable mongodb"
 systemctl enable mongod &>>${log_file}
 echo $?
@@ -15,3 +16,4 @@ print_head "Start mongodb"
 systemctl start mongod  &>>${log_file}
 echo $?
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+
