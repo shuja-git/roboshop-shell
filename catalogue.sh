a3f6d9f49533a9fdb80917864a6f738c820bef24
shuja <shuja.gmail.com>
2023-03-04 17:27:26 +0530

    dev added

--- catalogue.sh
+++ catalogue.sh
@@ -50,5 +50,5 @@ print_head "Installing mongo client"
 yum install mongodb-org-shell -y &>>"${log_file}"
 status_check $?
 print_head "Load Schema"
-mongo --host mongodb.shujadevops.online </app/schema/catalogue.js &>>${log_file}
+mongo --host mongodb-dev.shujadevops.online </app/schema/catalogue.js &>>${log_file}
 status_check $?
