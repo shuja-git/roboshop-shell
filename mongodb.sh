26c10fcfe1cafeb61c113a3de63b799c0422ca9f
shuja <shuja.gmail.com>
2023-03-04 15:23:34 +0530

    if condition added

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,26 @@
 source common.sh
 
-print_head "Setup mongodb repos"
+print_head "Setup mongodb repos" &>>${log_file}
 cp configs/mongo.repo /etc/yum.repos.d/mongo.repo
-
-print_head "Install Mongodb"
+if [ $? -q 0 ]; then
+    echo -e "\e[33m Success\e[0m"
+fi
+print_head "Install Mongodb" &>>${log_file}
 yum install mongodb-org -y
+if [ $? -q 0 ]; then
+    echo -e "\e[33m Success\e[0m"
+fi
 
-print_head "Enable mongodb"
+print_head "Enable mongodb" &>>${log_file}
 systemctl enable mongod
+if [ $? -q 0 ]; then
+    echo -e "\e[33m Success\e[0m"
+fi
 
-print_head "Start mongodb"
+print_head "Start mongodb" &>>${log_file}
 systemctl start mongod
+if [ $? -q 0 ]; then
+    echo -e "\e[33m Success\e[0m"
+fi
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
