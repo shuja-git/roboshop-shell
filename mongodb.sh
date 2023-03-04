e49802b6fdcc2c0e5ac3bc792585caeddf04dc15
shuja <shuja.gmail.com>
2023-03-04 15:30:33 +0530

    if cond

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,17 @@
 source common.sh
 
-print_head "Setup mongodb repos" &>>${log_file}
-cp configs/mongo.repo /etc/yum.repos.d/mongo.repo
-echo $?
+print_head "Setup mongodb repos"
+cp configs/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+if [ "$?" -eq 0 ]; then
+    echo success
+fi
 print_head "Install Mongodb" &>>${log_file}
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
 echo $?
-print_head "Enable mongodb" &>>${log_file}
-systemctl enable mongod
+print_head "Enable mongodb"
+systemctl enable mongod &>>${log_file}
 echo $?
-print_head "Start mongodb" &>>${log_file}
-systemctl start mongod
+print_head "Start mongodb"
+systemctl start mongod  &>>${log_file}
 echo $?
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
