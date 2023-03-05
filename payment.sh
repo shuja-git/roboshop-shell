7d5189849aa92a587d1dd69409c0852c62b98937
shuja <shuja.gmail.com>
2023-03-05 15:02:28 +0530

    variable name issue fixed

--- payment.sh
+++ payment.sh
@@ -1,8 +1,8 @@
 source common.sh
 
 
-roboshop_user_password=$1
-if [ -z "${roboshop_user_password}" ]; then
+roboshop_app_password=$1
+if [ -z "${roboshop_app_password}" ]; then
     echo -e "\e[31mMissing Roboshop User password argument\e[0m"
     exit 1
 fi
